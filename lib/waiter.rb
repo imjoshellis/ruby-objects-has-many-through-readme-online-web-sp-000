bd18701bc7f623f5a9b3c7a0f86f0801f93d9b91
Josh Ellis <joshe523@gmail.com>
2020-03-04 22:53:39 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,11 +2,11 @@ class Waiter
   attr_accessor :name, :experience
 
   @@all = []
-  
+
   def initialize(name, experience)
     @name = name
     @experience = experience
-    @@all << self  
+    @@all << self
   end
 
   def self.all
